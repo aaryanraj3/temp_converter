3c58fce38495616330e7279568717e35693e0eaa
Aaryan Raj <69566713+aaryanraj3@users.noreply.github.com>
2023-09-11 09:29:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function convertKelvin() {
     const kelvinInput = document.getElementById("kelvinInput").value;
-    const fahrenheit = (kelvinInput - 273.15) * 9/5 + 32;
+    const fahrenheit = (kelvinInput - 273.15) * 9 / 5 + 32;
     const celsius = kelvinInput - 273.15;
     document.getElementById("fahrenheitOutput").textContent = fahrenheit.toFixed(2) + " °F";
     document.getElementById("celsiusOutput").textContent = celsius.toFixed(2) + " °C";
@@ -8,8 +8,8 @@ function convertKelvin() {
 
 function convertFahrenheit() {
     const fahrenheitInput = document.getElementById("fahrenheitInput").value;
-    const kelvin = (fahrenheitInput - 32) * 5/9 + 273.15;
-    const celsius = (fahrenheitInput - 32) * 5/9;
+    const kelvin = (fahrenheitInput - 32) * 5 / 9 + 273.15;
+    const celsius = (fahrenheitInput - 32) * 5 / 9;
     document.getElementById("kelvinOutput").textContent = kelvin.toFixed(2) + " K";
     document.getElementById("celsiusOutput2").textContent = celsius.toFixed(2) + " °C";
 }
@@ -17,7 +17,7 @@ function convertFahrenheit() {
 function convertCelsius() {
     const celsiusInput = document.getElementById("celsiusInput").value;
     const kelvin = parseFloat(celsiusInput) + 273.15;
-    const fahrenheit = (parseFloat(celsiusInput) * 9/5) + 32;
+    const fahrenheit = (parseFloat(celsiusInput) * 9 / 5) + 32;
     document.getElementById("kelvinOutput2").textContent = kelvin.toFixed(2) + " K";
     document.getElementById("fahrenheitOutput2").textContent = fahrenheit.toFixed(2) + " °F";
 }
